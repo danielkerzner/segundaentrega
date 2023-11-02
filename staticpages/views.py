2abc145adbb4136e9a09f6773ef02a2a467a9950
Daniel Kerzner <daniel.kerzner@usp.br>
2023-11-02 18:10:04 -0300

    Criar site estático padrão

--- staticpages/views.py
+++ staticpages/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def home(request):
     return render(request, 'staticpages/home.html')
+
+def about(request):
+    return render(request, 'staticpages/about.html')
